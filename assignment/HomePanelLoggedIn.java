339576ff8b931497e8fd97e616ee297e26f94138
lovo@Lovison.id.au <lovo@Lovison.id.au>
2023-10-17 10:26:58 +1100

    basic login page created
    created a very basic login need will work on functionality later

--- assignment/HomePanelLoggedIn.java
+++ assignment/HomePanelLoggedIn.java
@@ -1,30 +1,51 @@
-// package assignment;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 class HomePanelLoggedIN extends SwitchablePanel {
-   private JPanel westPanel;
-   private JComboBox<String> optionsComboBox;
-      private JPanel northPanel;
+    private JPanel westPanel;
+    private JComboBox<String> optionsComboBox;
+    private JPanel northPanel;
     
     public HomePanelLoggedIN(MainAppFrame frame) {
         super(frame);
         addSpecificFeatures(); 
     }
 
-
+    @Override
     protected void addSpecificFeatures() {
-        
-        // JLabel label1 = new JLabel("LOGGEDIN");
-        // label1.setFont(new Font("Verdana", Font.PLAIN, 25));
-        // label1.add(label1, BorderLayout.WEST);
-        // // northPanel.add(optionsComboBox, BorderLayout.NORTH);
+        //layout setup
+        JPanel contentPanel = new JPanel(new BorderLayout());
 
-       
+        //title
+        JLabel label = new JLabel("Home Page", SwingConstants.CENTER);
+        label.setFont(new Font("Arial", Font.BOLD, 32));
+        contentPanel.add(label, BorderLayout.NORTH); 
+        add(contentPanel, BorderLayout.CENTER);
+
+        //panel to hold the username button, positioned to the top-right
+        // JPanel buttonPanel = new JPanel();
+        // JPanel buttonPanel1 = new JPanel(new BorderLayout());
+
+        // JButton username = new JButton("username");
+        // buttonPanel.add(username);
+        // buttonPanel1.add(buttonPanel, BorderLayout.NORTH);
+        // contentPanel.add(buttonPanel1, BorderLayout.EAST);
+ 
+        JPanel imagePanel = new JPanel(new GridLayout(4, 1, 5, 5));  // 4 rows, 1 column, 5px gaps
+
+        // assuming the images are stored in a folder named 'images' and have names like 'image1.jpg', 'image2.jpg', etc.
+        String[] imageTitles = {"Title 1", "Title 2", "Title 3", "Title 4"};
+        for (int i = 0; i < 4; i++) {
+            ImageIcon imageIcon = new ImageIcon("Pictures/Data1.jpg");
+            
+            JLabel imageLabel = new JLabel(imageIcon, SwingConstants.CENTER);
+            JLabel titleLabel = new JLabel(imageTitles[i], SwingConstants.CENTER);
+            JPanel singleImagePanel = new JPanel(new BorderLayout());
+            singleImagePanel.add(imageLabel, BorderLayout.CENTER);
+            singleImagePanel.add(titleLabel, BorderLayout.SOUTH);
+            imagePanel.add(singleImagePanel);
+        }
+
+        contentPanel.add(imagePanel, BorderLayout.CENTER);
     }
-
-
 }
--- assignment/ProfilePanel.java
+++ assignment/ProfilePanel.java
@@ -62,55 +62,5 @@ class ProfilePanel extends SwitchablePanel {
     }
 }
 
-class ProfilePanel extends SwitchablePanel {
-    private JTextField firstnameField;
-    private JTextField lastnameField;
-    private JTextField usernameField;
-    private JPasswordField passwordField;
-    private JButton editChangesButton;
-    private JButton saveChangesButton;
 
-    public ProfilePanel(MainAppFrame frame) {
-        super(frame);
-        addSpecificFeatures();
-    }
 
-    protected void addSpecificFeatures() {
-        JPanel contentPanel = new JPanel(new BorderLayout());
-
-        JLabel label = new JLabel("Profile", SwingConstants.CENTER);
-        label.setFont(new Font("Arial", Font.BOLD, 32));
-        contentPanel.add(label, BorderLayout.NORTH); 
-
-        Font customFont = new Font("Arial", Font.PLAIN, 24);
-
-        // Create a panel for user input fields
-        JPanel inputPanel = new JPanel(new GridLayout(4, 1));
-        addComponent(inputPanel, "First Name:", firstnameField = new JTextField(20), customFont);
-        addComponent(inputPanel, "Last Name:", lastnameField = new JTextField(20), customFont);
-        addComponent(inputPanel, "Username:", usernameField = new JTextField(20), customFont);
-        addComponent(inputPanel, "Password:", passwordField = new JPasswordField(20), customFont);
-        
-        contentPanel.add(inputPanel, BorderLayout.CENTER); 
-
-        // Create a panel for buttons
-        JPanel buttonPanel = new JPanel();
-        editChangesButton = new JButton("Edit Changes");
-        saveChangesButton = new JButton("Save Changes");
-        buttonPanel.add(editChangesButton);
-        buttonPanel.add(saveChangesButton);
-        
-        contentPanel.add(buttonPanel, BorderLayout.SOUTH); 
-
-        add(contentPanel, BorderLayout.CENTER); // Add contentPanel to the CENTER region of ProfilePanel
-    }
-
-    private void addComponent(JPanel panel, String labelText, JComponent component, Font customFont) {
-        JLabel label = new JLabel(labelText);
-        label.setFont(customFont);
-        panel.add(label);
-        
-        component.setFont(customFont);
-        panel.add(component);
-    }
-}
